2ee0b5372eb5a00d64bff0035a94e429976767cf
Burhan <burhanashraf5829@gmail.com>
2026-07-13 16:06:05 +0000

    @9:35 PM

---
+++ tf_for_scope/main.tf
@@ -0,0 +1,11 @@
+resource "google_observability_trace_scope" "observability_trace_scope" {
+  trace_scope_id = "test-scope1S"
+  location       = var.location
+
+  resource_names = [
+    for project_id in var.projects :
+    "projects/${project_id}"
+  ]
+
+  description = "A trace scope configured with Terraform"
+}
---
+++ tf_for_scope/output.tf
@@ -0,0 +1,3 @@
+output "trace_scope_id" {
+    value = google_observability_trace_scope.observability_trace_scope.trace_scope_id
+}
