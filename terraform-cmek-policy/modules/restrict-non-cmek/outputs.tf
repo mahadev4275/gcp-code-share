a6410a142fb7b153d77ab4577239af3a72a290bf
Aman <aman92121rastogi@gmail.com>
2026-07-18 11:37:27 +0000

    CMEK project org policy for Observabiity API

---
+++ terraform-cmek-policy/modules/restrict-non-cmek/outputs.tf
@@ -0,0 +1,3 @@
+output "policy_name" {
+  value = google_org_policy_policy.restrict_non_cmek_services.name
+}
---
+++ terraform-cmek-policy/modules/restrict-non-cmek/variables.tf
@@ -0,0 +1,7 @@
+variable "project_id" {
+  type = string
+}
+
+variable "restricted_services" {
+  type = list(string)
+}
