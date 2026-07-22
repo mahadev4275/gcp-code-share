763fc9e99c4daa3893fe235cefaeb6fd0893f580
Aman <aman92121rastogi@gmail.com>
2026-07-22 11:58:04 +0000

    SQL Based Cloud Alerting

---
+++ sql-alert-module/main.tf
@@ -0,0 +1,30 @@
+resource "google_project_service" "monitoring" {
+  project = var.project_id
+  service = "monitoring.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+module "sql_alert" {
+
+  source = "./modules/sql_alert"
+
+  alert_name = "Critical Error Logs Alert"
+
+  notification_email = "aman.rastogi01@infosys.com"
+
+  query = <<EOF
+SELECT *
+FROM `${var.project_id}.global._Default._AllLogs`
+WHERE severity IN ("ERROR","CRITICAL")
+
+EOF
+
+  periodicity = 600
+
+  threshold = "0"
+
+  depends_on = [
+    google_project_service.monitoring
+  ]
+}
