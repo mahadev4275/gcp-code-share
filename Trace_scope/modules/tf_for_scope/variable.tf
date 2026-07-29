881c24e6005e311c40915a20242a9b0b811782ce
Burhanashraf98 <burhanashraf5829@gmail.com>
2026-07-29 18:08:24 +0530

    Update variable.tf

--- Trace_scope/modules/tf_for_scope/variable.tf
+++ Trace_scope/modules/tf_for_scope/variable.tf
@@ -3,11 +3,6 @@ variable "project" {
   type = string
 }
 
-variable "projects" {
-  type        = list(string)
-  description = "List of project IDs to include in the trace scope"
-  default     = []
-}
 
 variable "location" {
   type = string
